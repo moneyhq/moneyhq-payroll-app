5be5068c088e4ca6af3050d4eba31e35ab5b3c71
Temi Adenuga <a.adenuga@hotmail.com>
2024-06-26 23:22:56 +0100

    Setup frontend, create side nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import "./styles/App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SideNav from "./components/MainNav/MainNav";
+import TopNav from "./components/TopNav/TopNav";
+import Home from "./pages/Home/Home";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <TopNav />
+      <SideNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
