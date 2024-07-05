06ea64c6a19b69a596d5fc8050868545549c73e9
Temi Adenuga <a.adenuga@hotmail.com>
2024-07-05 13:59:47 +0100

    Add more elements to dashboard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,8 @@ import {
   faArrowTrendDown,
   faUsers,
   faArrowRight,
+  faCalendarDays,
+  faGavel,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
@@ -66,7 +68,7 @@ export default function Home() {
           <div className="home-box__currentdata home-box__currentdata--tertiary">
             <div className="home-box__currentdata home-box__currentdata--var1">
               <div className="home-box__currentdata--title">
-                Upcoming Payroll
+                Upcoming Salary Amount
               </div>
               <div className="home-box__currentdata--salary-amount">
                 $150,000.00
@@ -92,29 +94,90 @@ export default function Home() {
             </div>
           </div>
           <div className="home-box__wrapper home-box__wrapper--primary">
-            <div className="home-box__card home-box__card--primary">
-              <div className="home-box__card-top">
-                <div className="home-box__card-top--left">
-                  <span>Employees</span>
-                  <h2>27</h2>
+            <div className="card card-upcomingdate">
+              <div className="card-top">
+                <div className="card-top__left">
+                  <span className="card-top__left-title">
+                    Upcoming Sal Date
+                  </span>
+                  <h2 className="card-top__left-data">
+                    25 Jul <span className="card-top__left-data2">2024</span>
+                  </h2>
                 </div>
-                <div className="home-box__card-top--right">
-                  <FontAwesomeIcon icon={faUsers} />
+                <div className="card-top__right">
+                  <FontAwesomeIcon icon={faCalendarDays} />
                 </div>
               </div>
-              <Link to="/employees" className="home-box__card-bottom">
-                View
+              <Link to="/employees" className="card-bottom">
+                Run Payroll
                 <FontAwesomeIcon
                   icon={faArrowRight}
-                  className="home-box__card-bottom--icon"
+                  className="card-bottom--icon"
                 />
               </Link>
             </div>
-            <div className="home-box__card home-box__card--primary"></div>
-            <div className="home-box__card home-box__card--primary"></div>
+            <div className="card card-employees">
+              <div className="card-top">
+                <div className="card-top__left">
+                  <span className="card-top__left-title">Employees</span>
+                  <h2 className="card-top__left-data">27</h2>
+                </div>
+                <div className="card-top__right">
+                  <FontAwesomeIcon icon={faUsers} />
+                </div>
+              </div>
+              <Link to="/employees" className="card-bottom">
+                View
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  className="card-bottom--icon"
+                />
+              </Link>
+            </div>
+            <div className="card card-compliance">
+              <div className="card-events__title">
+                <FontAwesomeIcon
+                  icon={faGavel}
+                  className="card-events__title--icon"
+                />
+                Compliance
+              </div>
+              <div className="card-compliance__body">
+                <div className="card-compliance__left">
+                  <div className="card-compliance__data card-compliance__left-top">
+                    <span className="card-compliance__title">Tax (PAYE)</span>
+                    <FontAwesomeIcon
+                      icon={faArrowRight}
+                      className="card-compliance__icon"
+                    />
+                    <span className="card-compliance__value">$8,500</span>
+                  </div>
+                  <div className="card-compliance__data card-compliance__left-bottom">
+                    <span className="card-compliance__title">Pension</span>
+                    <FontAwesomeIcon
+                      icon={faArrowRight}
+                      className="card-compliance__icon"
+                    />
+                    <span className="card-compliance__value">$8,500</span>
+                  </div>
+                </div>
+                {/* <div className="card-top__right">
+                  <FontAwesomeIcon icon={faGavel} />
+                </div> */}
+              </div>
+            </div>
+            <div className="card card-events">
+              <div className="card-events__title">
+                <FontAwesomeIcon
+                  icon={faCalendarDays}
+                  className="card-events__title--icon"
+                />
+                Upcoming events
+              </div>
+            </div>
           </div>
           <div className="home-box__wrapper home-box__wrapper--secondary">
-            <div className="home-box__card home-box__card--secondary"></div>
+            <div className="card card--secondary"></div>
           </div>
         </div>
       </div>
