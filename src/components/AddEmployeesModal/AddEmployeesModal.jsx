26d9b9fc6748aa24093774240671c7f60a4dc0e7
Temi Adenuga <a.adenuga@hotmail.com>
2024-07-09 09:12:17 +0100

    Add second page of the addEmployee modal

--- src/components/AddEmployeesModal/AddEmployeesModal.jsx
+++ src/components/AddEmployeesModal/AddEmployeesModal.jsx
@@ -9,9 +9,54 @@ import Dropdown from "../Dropdown/Dropdown";
 export default function AddEmployeesModal() {
   const { closeModal } = useModal();
   const [isInviteChecked, setIsInviteChecked] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [employeeFormData, setEmployeeFormData] = useState({
+    employeeFirstName: "",
+    employeeLastName: "",
+    inviteEmployee: "",
+    employeeEmail: "",
+    employeeDateOfBirth: "",
+    employeeAddress: "",
+    employeeState: "",
+    employeeJoinDate: "",
+    employeeSalary: "",
+    employeePayFrequency: "",
+    employeePensionAdmin: "",
+    employeeTaxId: "",
+    employeePensionId: "",
+  });
+
+  const {
+    employeeFirstName,
+    employeeLastName,
+    inviteEmployee,
+    employeeEmail,
+    employeeDateOfBirth,
+    employeeAddress,
+    employeeState,
+    employeeJoinDate,
+    employeeSalary,
+    employeePayFrequency,
+    employeePensionAdmin,
+    employeeTaxId,
+    employeePensionId,
+  } = employeeFormData;
 
   const handleCheckboxChange = () => {
     setIsInviteChecked(!isInviteChecked);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value, checked } = event.target;
+    console.log(event.target);
+    setEmployeeFormData({ ...employeeFormData, [name]: value });
+    console.log(name, value, checked);
+  };
+
+  const handleNext = () => {
+    if (currentPage === 1) {
+      setCurrentPage(2);
+    }
   };
 
   const EmployeeBasicInfo = () => {
@@ -32,7 +77,8 @@ export default function AddEmployeesModal() {
                 type="text"
                 name="employeeFirstName"
                 id="employeeFirstName"
-                required
+                onChange={handleInputChange}
+                value={employeeFirstName}
               />
             </div>
             <div className="add-employees-modal__item add-employees-modal__item--lastname">
@@ -49,6 +95,8 @@ export default function AddEmployeesModal() {
                 type="text"
                 name="employeeLastName"
                 id="employeeLastName"
+                value={employeeLastName}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -168,11 +216,154 @@ export default function AddEmployeesModal() {
         </div>
 
         <div className="add-employees-modal__form-button">
+          {/* <div className="button-secondary add-employees-modal__form-back">
+            Back
+          </div> */}
+          <div
+            className="button-primary add-employees-modal__form-next"
+            onClick={handleNext}
+          >
+            Next
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const EmployeePayDetails = () => {
+    return (
+      <div className="add-employees-modal__form-box">
+        <div className="add-employees-modal__form-container">
+          <div className="add-employees-modal__item add-employees-modal__item--joindate">
+            <label
+              className="add-employees-modal__label"
+              htmlFor="employeeJoinDate"
+            >
+              Join Date <span className="add-employees-modal__required">*</span>
+            </label>
+            <input
+              className="add-employees-modal__input"
+              type="date"
+              name="employeeJoinDate"
+              id="employeeJoinDate"
+            />
+          </div>
+          <div className="add-employees-modal__form-wrapper add-employee-modal__form-wrapper--salary">
+            <div className="add-employees-modal__item add-employees-modal__item--salary">
+              <label
+                className="add-employees-modal__label"
+                htmlFor="employeeSalary"
+              >
+                Salary <span className="add-employees-modal__required">*</span>
+              </label>
+              <input
+                className="add-employees-modal__input
+              "
+                type="number"
+                name="employeeSalary"
+                id="employeeSalary"
+                min="50"
+                placeholder="1000"
+              />
+            </div>
+            <div className="add-employees-modal__item add-employees-modal__item--frequency">
+              <div className="add-employees-modal__label">
+                Pay Frequency
+                <span className="add-employees-modal__required">*</span>
+              </div>
+              <Dropdown
+                options={[{ value: "monthly", label: "Monthly" }]}
+                defaultLabel={"Monthly"}
+              />
+            </div>
+          </div>
+          <div className="add-employees-modal__item add-employees-modal__item--taxid">
+            <label
+              className="add-employees-modal__label"
+              htmlFor="employeeTaxId"
+            >
+              Tax ID
+              <span
+                className={
+                  isInviteChecked
+                    ? "add-employees-modal__optional"
+                    : "add-employees-modal__required"
+                }
+              >
+                {isInviteChecked ? " (Optional)" : " *"}
+              </span>
+            </label>
+            <input
+              className="add-employees-modal__input"
+              type="text"
+              name="employeeTaxId"
+              id="employeeTaxId"
+              placeholder="N-123456"
+            />
+          </div>
+          <div className="add-employees-modal__item add-employees-modal__item--pfa">
+            <div className="add-employees-modal__label" htmlFor="employeePfa">
+              Pension Fund Administrator
+              <span
+                className={
+                  isInviteChecked
+                    ? "add-employees-modal__optional"
+                    : "add-employees-modal__required"
+                }
+              >
+                {isInviteChecked ? " (Optional)" : " *"}
+              </span>
+            </div>
+            <Dropdown
+              options={[
+                {
+                  value: "Stanbic IBTC Pension",
+                  label: "Stanbic IBTC Pension",
+                },
+                {
+                  value: "ARM Pensions",
+                  label: "ARM Pensions",
+                },
+                {
+                  value: "Leadway Pensure",
+                  label: "Leadway Pensure",
+                },
+              ]}
+              defaultLabel={"ARM Pensions"}
+            />
+          </div>
+          <div className="add-employees-modal__item add-employees-modal__item--pensionid">
+            <label
+              className="add-employees-modal__label"
+              htmlFor="employeePensionId"
+            >
+              Pension ID
+              <span
+                className={
+                  isInviteChecked
+                    ? "add-employees-modal__optional"
+                    : "add-employees-modal__required"
+                }
+              >
+                {isInviteChecked ? " (Optional)" : " *"}
+              </span>
+            </label>
+            <input
+              className="add-employees-modal__input"
+              type="text"
+              name="employeePensionId"
+              id="employeePensionId"
+              placeholder="PEN111234567890"
+            />
+          </div>
+        </div>
+
+        <div className="add-employees-modal__form-button">
           <div className="button-secondary add-employees-modal__form-back">
             Back
           </div>
           <div className="button-primary add-employees-modal__form-next">
-            Next
+            Submit
           </div>
         </div>
       </div>
@@ -182,7 +373,10 @@ export default function AddEmployeesModal() {
   return (
     <div className="add-employees-modal">
       <div className="add-employees-modal__title">Add Employee</div>
-      <form className="add-employees-modal__form">{<EmployeeBasicInfo />}</form>
+      <form className="add-employees-modal__form">
+        {currentPage === 1 && <EmployeeBasicInfo />}
+        {currentPage === 2 && <EmployeePayDetails />}
+      </form>
       {/* <button onClick={() => closeModal("addEmployeesModal")}>Close</button> */}
     </div>
   );
